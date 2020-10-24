f17f736583c4151642beb61a79b06a3c9c84f5f1
Anubhav Gupta <anubhav.gupta.cse19@itbhu.ac.in>
2020-10-24 19:16:11 +0530

    Fixed Linking between Static Files

--- ridham/ridham/urls.py
+++ ridham/ridham/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('login/', include('login_module.urls'))
+    path('app/', include('login_module.urls'))
 
 ]
