e59cf7573d8c07885feb2663f788302c3c3b4c5c
Anubhav Gupta <anubhav.gupta.cse19@itbhu.ac.in>
2020-10-24 20:01:58 +0530

    Implemented Basic Login Functionality

--- ridham/login_module/views.py
+++ ridham/login_module/views.py
@@ -1,9 +1,27 @@
 from django.http import request
 from django.shortcuts import render
-
-# Create your views here.
+from .models import User, LoginHistory
 def login(request):
-    return render(request, 'login_module/loginFile.html')
+    if request.method == 'GET':
+        return render(request, 'login_module/loginFile.html')
+    else:
+        try:
+            lgtry = request.POST['lgtry']
+        except:
+            return render(request, 'login_module/loginFile.html')
+        if lgtry[0] == '1':
+            userInputUserName = request.POST['username']
+            userInputPassword = request.POST['password']
+            print(userInputPassword, userInputUserName)
+            #TODO : Proper Exception Handling for invalid cases
+            try:
+                user = User.objects.get(userName=userInputUserName, passWord=userInputPassword)
+            except User.DoesNotExist:
+                return render(request, 'login_module/loginFile.html')
+            return render(request, 'login_module/loginSuccess.html')
+
+        else:
+            return render(request, 'login_module/loginFile.html')
 
 def forgotPassword(request):
     return render(request, 'login_module/forgotPassword.html')
