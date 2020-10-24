b9fdc7557fe0c7a2c4e04a8860c0256964d9a1f0
Anant Saxena <anantsaxena.official@gmail.com>
2020-10-25 03:40:31 +0530

    Song object added

--- ridham/login_module/models.py
+++ ridham/login_module/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class User(models.Model):
     userName = models.CharField(max_length=32)
     passWord = models.CharField(max_length=32)
@@ -8,6 +9,17 @@ class User(models.Model):
     isBlocked = models.BooleanField()
     def __str__(self) -> str:
         return self.userName
+    def get_song_list(self):
+        return Song.objects.filter(owner = self)
+    
+
+class Song(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    songName = models.CharField(max_length=64)
+    songLink = models.CharField(max_length=512)
+
+    def __str__(self) -> str:
+        return self.songName
 
 class LoginHistory(models.Model):
     loginTime = models.DateTimeField()
@@ -15,3 +27,5 @@ class LoginHistory(models.Model):
     loginIp = models.CharField(max_length=20)
     def __str__(self) -> str:
         return self.userName
+
+
