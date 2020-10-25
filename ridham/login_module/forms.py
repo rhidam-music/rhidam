55f6ceadf33f76d0acf1aa29476b1d7147e386d6
Anant Saxena <anantsaxena.official@gmail.com>
2020-10-25 23:53:39 +0530

    user model now uses django auth

---
+++ ridham/login_module/forms.py
@@ -0,0 +1,19 @@
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from django.contrib.auth.models import User
+from .models import Forgot
+from django.forms import ModelForm
+
+
+class CreateUserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+
+class ForgotForm(ModelForm):
+    class Meta:
+        model  = Forgot
+        fields = ['username', 'email']
+    
