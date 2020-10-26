47d19cb719bcc6a72ce48a2b8309b19c2e8bfbff
Anant Saxena <anantsaxena.official@gmail.com>
2020-10-26 15:45:21 +0530

    latest fixed version

--- ridham/login_module/forms.py
+++ ridham/login_module/forms.py
@@ -9,7 +9,7 @@ from django.forms import ModelForm
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['username', 'first_name', 'last_name',  'email', 'password1', 'password2']
 
 
 class ForgotForm(ModelForm):
