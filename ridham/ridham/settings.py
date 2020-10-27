b37d63154ce20340cabafee399326a623152e06a
Anant Saxena <anantsaxena.official@gmail.com>
2020-10-27 18:00:49 +0530

    started working on user profile

--- ridham/ridham/settings.py
+++ ridham/ridham/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -136,3 +137,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'rhidam.helpdesk@gmail.com'
 EMAIL_HOST_PASSWORD = 'gta5GTA5'
 
+MEDIA_URL = '/images/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
